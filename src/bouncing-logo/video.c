6933e317787f98d273262604d69a0f2462682f5f
jameswilddev <jameswilddev@outlook.com>
2020-12-27 23:30:14 +0000

    Draw logo.

--- src/bouncing-logo/video.c
+++ src/bouncing-logo/video.c
@@ -1,7 +1,8 @@
 #include "video.h"
 #include "state.h"
+#include "logo.ase.h"
 
 VIDEO_IMPLEMENTATION
 {
-  video_buffer[0]++;
+  sprite_animation_draw(logo_rainbow, state_elapsed_buffer, state_x_buffer, state_y_buffer, video_buffer, video_width, video_height);
 }
